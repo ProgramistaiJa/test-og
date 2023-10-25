d8a3124389398619135041f397fe85dc157298a0
Artur Wojciechowski <programista@i-ja.pl>
2023-10-25 02:02:57 +0200

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='bg-slate-900 text-white max-w-3xl mx-auto py-20 px-4 '>
+      <body>{children}</body>
     </html>
   )
 }
